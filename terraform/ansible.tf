8cfe8b88ec7d534b4315d554d37dabea5eb0a731
Denis Simankin <x-modem@yandex.ru>
2022-03-22 15:51:30 +0300

    c5

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -8,24 +8,23 @@ resource "null_resource" "wait" {
   ]
 }
 
-resource "null_resource" "roles_install" {
-  provisioner "local-exec" {
-    command      = "ansible-galaxy install -r ../requirements.yml --roles-path ../roles"
-  }
-  depends_on = [
-    null_resource.wait
-  ]
-}
+#resource "null_resource" "roles_install" {
+#  provisioner "local-exec" {
+#    command      = "ansible-galaxy install -r ../requirements.yml --roles-path ../roles"
+#  }
+#  depends_on = [
+#    null_resource.wait
+#  ]
+#}
 
 
-resource "null_resource" "cluster" {
-  provisioner "local-exec" {
-    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../inventory/prod/hosts.yml ../site.yml"
-  }
-
-  depends_on = [
-    null_resource.roles_install
-  ]
-}
+#resource "null_resource" "cluster" {
+#  provisioner "local-exec" {
+#    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../inventory/prod/hosts.yml ../site.yml"
+#  }
+#  depends_on = [
+#    null_resource.roles_install
+#  ]
+#}
 
 
